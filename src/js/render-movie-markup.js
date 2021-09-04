c1cbe814c1de39034aaaf69c5937bb56f2197848
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-09-04 03:07:23 +0300

    add render-movie-markup and refs.js, card.hbs

---
+++ src/js/render-movie-markup.js
@@ -0,0 +1,8 @@
+import movieCardTmpl from './template/card.hbs';
+import refs from './refs';
+
+export default function renderMovies (data) {
+  const markup = movieCardTmpl(data);
+
+  refs.movieStorage.insertAdjacentHTML('beforeend', markup);
+}
