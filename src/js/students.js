897ab7a76244fbbb5bc14e637de0a13f75df2797
Visal Rahmanov <rahmanovvisal@gmail.com>
2021-09-05 19:20:24 +0300

    вносит правки на модалку

--- src/js/students.js
+++ src/js/students.js
@@ -10,22 +10,28 @@ const refs = {
 
 refs.studentGoit.addEventListener('click', onOpenModal);
 refs.btnClose.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onCloseModalBackdrop);
+
 
 renderTeamModal(students);
 
 function onOpenModal() {
+  
   window.addEventListener('keydown', onCloseModal);
   refs.backdrop.classList.remove('is-hidden');
   document.body.classList.add('modal-open');
 }
 
 function onCloseModal(event) {
-  window.onscroll = function () {
-    return false;
-  };
+  document.body.classList.remove('modal-open');
   refs.backdrop.classList.add('is-hidden');
   if (event.code === 'Escape') {
     window.removeEventListener('keydown', onCloseModal);
+  }
+}
+function onCloseModalBackdrop(event) {
+  if (event.target === event.currentTarget) {
+    onCloseModal();
   }
 }
 function renderTeamModal(data) {
