c7606cf1b2add56fc6e4597d18a2601e22402aa3
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-09-05 12:32:23 +0300

    add class to fetchMovies.js

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,37 +1,81 @@
+// ==================================================
 const API_KEY = '74b3d185775f996114b8f83bcbb83c33';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+// ==================================================
 
-/* Приходят объекты, из которых деструктуризируем results
-и уже их используем в паре с шаблонизатором для создания разметки */
+class FilmApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.currentPage = 1;
+  }
+// ==========  By Genres  ==========
+  fetchGenres() {
+  const url =`${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`;
 
-function getPopularMovies() {
-  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
   return fetch(url)
-    .then(response => response.json())
-    .then(({ results }) => {
-      console.log(results);
+    
+    .then(responce => responce.json())
+    
+    .then(data => {
+      return data.genres;
     })
-    .catch(console.log);
-}
+  }
+// ==========  By Trend  ==========
+  fetchTrendingMovies(currentPage = 1) {
+    const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`;
 
-/* Приходят объекты, из которых деструктуризируем results 
-и уже их используем в паре с шаблонизатором для создания разметки */
-
-function getSpecificMovie(movieTitle) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieTitle}`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(({ results }) => {
-      console.log(results);
+    return fetch(url)
+    
+    .then(responce => responce.json())
+    
+    .then(data => {
+      return data.results;
     })
-    .catch(console.log);
-}
+  }
+// ==========  By Keyword  ==========
+  fetchMoviesByKeyWord(query, currentPage = 1) {
+    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${currentPage}&include_adult=false`;
 
-/* Приходит объект с описанием конкретного фильма */
+    return fetch(url)
+    
+    .then(responce => responce.json())
+    
+    .then(data => {
+      return data.results;
+    })
+  }
+// ==========  By ID  ==========
+  fetchMoviesById(id) {
+    const url =`${BASE_URL}movie/${id}?api_key=${API_KEY}&language=en-US`;
 
-function getMovieDescription(movieId) {
-  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(console.log)
-    .catch(console.log);
+    return fetch(url)
+    
+    .then(r => r.json())
+    
+    .then(data => {
+      return {
+        id: data.id,
+        title: data.original_title,
+        genres: data.genres.slice(0, 2).map(({ name }) => name),
+        about: data.overview,
+        popularity: data.popularity,
+        vote: data.vote_average,
+        votes: data.vote_count,
+        release: data.release_date.substring(0, 4),
+        poster_path: data.poster_path,
+      };
+    })
+  }
+
+  resetPage() {
+    this.currentPage = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
