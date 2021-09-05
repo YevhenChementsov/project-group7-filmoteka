ae38cb22990ef293f3a5957d6275e48f56fe5963
Visal Rahmanov <rahmanovvisal@gmail.com>
2021-09-05 20:52:40 +0300

    добавляет рефы

--- src/js/students.js
+++ src/js/students.js
@@ -2,42 +2,29 @@ import students from './studentData.json';
 import studentsTemplate from '../templates/students-modal.hbs';
 import Refs from './refs';
 
-<<<<<<< HEAD
-const refs = {
-  studentGoit: document.querySelector('.text-company-link'),
-  backdrop: document.querySelector('.backdrop-team'),
-  team: document.querySelector('.team-modal'),
-  btnClose: document.querySelector('.modal-close-btn'),
-};
-
 refs.studentGoit.addEventListener('click', onOpenModal);
 refs.btnClose.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onCloseModalBackdrop);
 
-=======
 Refs.studentGoit.addEventListener('click', onOpenModal);
 Refs.btnClose.addEventListener('click', onCloseModal);
->>>>>>> dev
 
 renderTeamModal(students);
 
 function onOpenModal() {
-  
   window.addEventListener('keydown', onCloseModal);
   Refs.backdrop.classList.remove('is-hidden');
   document.body.classList.add('modal-open');
 }
 
 function onCloseModal(event) {
-<<<<<<< HEAD
   document.body.classList.remove('modal-open');
   refs.backdrop.classList.add('is-hidden');
-=======
+
   window.onscroll = function () {
     return false;
   };
   Refs.backdrop.classList.add('is-hidden');
->>>>>>> dev
   if (event.code === 'Escape') {
     window.removeEventListener('keydown', onCloseModal);
   }
