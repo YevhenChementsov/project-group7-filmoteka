4d5d258657166c78b651fa94f074c6a3f13c7fd5
hooliganfromtroya <hooliganfromtroya@gmail.com>
2021-09-06 17:18:14 +0300

    header js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import './js/students';
 import './js/components/pagination/pagination';
+import './js/components/header/header';
 import './js/defaultPage';
 import './js/theme-switch';
--- src/js/refs.js
+++ src/js/refs.js
@@ -4,6 +4,7 @@ const Refs = {
   backdrop: document.querySelector('.backdrop-team'),
   team: document.querySelector('.team-modal'),
   btnClose: document.querySelector('.modal-close-btn'),
+  inputSearch: document.querySelector('.header__search-content input'),
 };
 
 export default Refs;
