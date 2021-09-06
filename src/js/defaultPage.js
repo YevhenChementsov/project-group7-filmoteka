a6226e19fe8dc4fa9befa6f94f8edbd78e9d63a2
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-09-06 22:39:17 +0300

    правка default

--- src/js/defaultPage.js
+++ src/js/defaultPage.js
@@ -3,10 +3,8 @@ import movieCardTmpl from '../templates/card.hbs';
 import FetchMovieApi from './apiMoviesService';
 // import appendMoviesMarkUp from './markup';
 
-
 const movieSearch = new FetchMovieApi();
 const initial = movieSearch.initialPage;
-
 
 export default async function showPopularMoviesByDefault(page) {
   const movies = await movieSearch.fetchTrendingMovies(page);
@@ -16,4 +14,3 @@ export default async function showPopularMoviesByDefault(page) {
 }
 
 showPopularMoviesByDefault(initial);
-
