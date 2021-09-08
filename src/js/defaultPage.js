d3ba41051e14ddcfd4ae4ad27866d4dce4281137
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-09-08 15:47:47 +0300

    add average vote to the movie,card.scss and card.hbs

--- src/js/defaultPage.js
+++ src/js/defaultPage.js
@@ -12,12 +12,13 @@ export default async function showPopularMoviesByDefault(page) {
     const { genre_ids } = movie;
     return {
       ...movie,
+      vote: movie.vote_average,
       year: movie.release_date ? movie.release_date.split('-')[0] : movie.first_air_date.split('-')[0],
       genre_ids: [
         ...genres
           .filter(({ id }) => genre_ids.includes(id))
           .map(({ name }) => name)
-          .slice(0, 3),
+          .slice(0, 2),
       ],
     };
   });
