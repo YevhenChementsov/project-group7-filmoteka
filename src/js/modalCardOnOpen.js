347ffbf0eb12ece2822c0749d4b02758a0430882
Ilya-Zaporjec <ilya.zaporojec@gmail.com>
2021-09-08 19:54:39 +0300

    add

--- src/js/modalCardOnOpen.js
+++ src/js/modalCardOnOpen.js
@@ -3,11 +3,15 @@ import Refs from './refs';
 import temp from '../templates/cardModal.hbs';
 import appendMoviesMarkUp from './markup';
 
+let watchedMovies = [];
+let queueMovies = [];
+
 Refs.movieStorage.addEventListener('click', event => {
   event.preventDefault();
 
   openModal(event.target);
 });
+
 Refs.modalCardsCloseBtn.addEventListener('click', event => {
   event.preventDefault();
   closeModal(event.target);
@@ -15,15 +19,16 @@ Refs.modalCardsCloseBtn.addEventListener('click', event => {
 
 function closeModal(event) {
   Refs.backdropModalCard.classList.add('is-hidden');
+  Refs.movieModal.removeEventListener('click', never);
   Refs.movieModal.innerHTML = '';
   if (event.code === 'Escape') {
     window.removeEventListener('keydown', closeModal());
   }
 }
+
 async function openModal(target) {
   Refs.backdropModalCard.classList.remove('is-hidden');
   const fetchedMovies = await showPopularMoviesByDefault();
-
   const movieCardInfo = fetchedMovies.filter(movie => {
     if (movie.id.toString() === target.dataset.id) {
       return movie;
@@ -32,24 +37,43 @@ async function openModal(target) {
   });
 
   appendMoviesMarkUp(Refs.movieModal, ...movieCardInfo, temp);
+  Refs.movieModal.addEventListener('click', never);
+}
 
-  Refs.movieModal.addEventListener('click', e => {
-    if (e.target.dataset.value === 'watched') {
-      addMoviesToWatchedLibrary(movieCardInfo);
-    } else if (e.target.dataset.value === 'queue') {
-      addMoviesToQueueLibrary(movieCardInfo);
+async function never(e) {
+  const image = document.querySelector('.modal-card .js-movie__image');
+  const fetchedMovies = await showPopularMoviesByDefault();
+  const movieCardInfo = fetchedMovies.filter(movie => {
+    if (movie.id.toString() === image.dataset.id) {
+      return movie;
     }
+    return;
   });
+
+  if (e.target.dataset.value === 'watched') {
+    e.target.textContent = 'Added to Watched!';
+    e.target.style.backgroundColor = 'yellow';
+    addMoviesToWatchedLibrary(...movieCardInfo);
+  } else if (e.target.dataset.value === 'queue') {
+    e.target.textContent = 'Added to Watched!';
+    e.target.style.backgroundColor = 'yellow';
+    addMoviesToQueueLibrary(...movieCardInfo);
+  }
 }
 
 async function addMoviesToWatchedLibrary(movie) {
-  let watchedMovies = [];
   watchedMovies.push(movie);
+  watchedMovies.filter(elem => {
+    if (elem.id === movie.id) {
+      return elem;
+    }
+    return;
+  });
   const movies = JSON.stringify(watchedMovies);
   localStorage.setItem('watchedMovies', movies);
 }
+
 async function addMoviesToQueueLibrary(movie) {
-  let queueMovies = [];
   queueMovies.push(movie);
   const movies = JSON.stringify(queueMovies);
   localStorage.setItem('queueMovies', movies);
