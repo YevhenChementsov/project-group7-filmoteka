e102bd3e298556b50fec87d94a63c9e448714de4
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-09-10 17:57:40 +0300

    убрал консоли 2.0

--- src/js/defaultPage.js
+++ src/js/defaultPage.js
@@ -19,12 +19,11 @@ function openHomepage() {
 
 function isButtonActive(event) {
   const active = document.querySelector('.active-link');
-    console.log(active);
 
-    if (active) {
-        active.classList.remove('active-link');
-    }
-    event.target.classList.add('active-link');
+  if (active) {
+    active.classList.remove('active-link');
+  }
+  event.target.classList.add('active-link');
 }
 
 const initial = API.initialPage;
@@ -36,7 +35,9 @@ export default async function showPopularMoviesByDefault(page) {
     return {
       ...movie,
       vote: movie.vote_average,
-      year: movie.release_date ? movie.release_date.split('-')[0] : movie.first_air_date.split('-')[0],
+      year: movie.release_date
+        ? movie.release_date.split('-')[0]
+        : movie.first_air_date.split('-')[0],
       genres: [
         ...genres
           .filter(({ id }) => genre_ids.includes(id))
